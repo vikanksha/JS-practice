3ed29bf54dea97cdcf22dab011696b7bf9bdf903
Vikanksha <vikanksha@Manishs-MacBook-Air-2.local>
2020-02-15 14:06:18 +0530

    program to compute the absolute difference between a specified number and 19.

---
+++ JS-practice/pg_8.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to compute the absolute difference between a specified number and 19. 
+//Returns triple their absolute difference if the specified number is greater than 19.
+function sum(a,b){
+    if (a==b){
+        return (a+b)*3;
+    }
+    else{
+        return (a+b);
+    }
+}
+console.log(sum(5,4))
