3d94e6a1322f6325f50eeddbbb0ad540f9a9fcd5
Vikanksha <vikanksha@Manishs-MacBook-Air-2.local>
2020-02-15 12:58:35 +0530

    repeat string n times

---
+++ JS-practice/repeat_string.js
@@ -0,0 +1,11 @@
+function repeatStringNumTimes(str, num) {
+    // repeat after me
+    let output = '';
+    for(let i = 0; i < num; i++) {
+    output+=str;
+    
+  }
+  return output;
+  }
+console.log(repeatStringNumTimes("abc", 3));
+  
