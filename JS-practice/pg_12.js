f4417a59627043555a514b548756150c3caa9091
Vikanksha <vikanksha@Manishs-MacBook-Air-2.local>
2020-02-15 13:44:22 +0530

    create a new string from a given string with the first character of the given string added at the front and back.

---
+++ JS-practice/pg_12.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+//function shift(str,pos){
+  //  first = str.substring(0,1);
+    //return first + str + first;
+//}//
+//console.log(shift("manish",0));
+
+function camelcase(s){
+    return(s , '\n');
+
+console.log(camelcase("TheMonDay"));
+}
